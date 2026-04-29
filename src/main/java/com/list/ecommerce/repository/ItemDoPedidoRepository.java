ebd58aee16c922c2e235c249b7932fa960a53bd5
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2026-04-29 08:08:41 -0300

    fix: remove notations

--- src/main/java/com/list/ecommerce/repository/ItemDoPedidoRepository.java
+++ src/main/java/com/list/ecommerce/repository/ItemDoPedidoRepository.java
@@ -7,8 +7,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
-import java.util.Optional;
+
 
 public interface ItemDoPedidoRepository extends JpaRepository<ItemDoPedido, ItemDoPedidoPK> {
 
